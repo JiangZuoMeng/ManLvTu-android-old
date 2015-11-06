105d1ed0ccae6f11d0f04ce5a4f0c031b16b3bbc
ekuri <wuwy23@mail2.sysu.edu.cn>
2015-11-06 16:48:00 +0800

    add marker for single map activity and list view long clicked listener

--- code/android/TravelMap/app/src/main/java/com/jiangzuomeng/travelmap/SingleTravelActivity.java
+++ code/android/TravelMap/app/src/main/java/com/jiangzuomeng/travelmap/SingleTravelActivity.java
@@ -1,17 +1,35 @@
 package com.jiangzuomeng.travelmap;
 
+import android.app.ActionBar;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 
 import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.MarkerOptions;
+import com.amap.api.maps2d.model.Polyline;
+import com.amap.api.maps2d.model.PolylineOptions;
 import com.jiangzuomeng.Adapter.SingleTravelItemListViewAdapter;
 
-public class SingleTravelActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SingleTravelActivity
+        extends AppCompatActivity
+        implements AMap.OnMapClickListener, AdapterView.OnItemLongClickListener {
 
     private MapView mapView;
     private AMap aMap;
+    private Polyline polyline;
+    private ArrayList markersLocation = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +38,8 @@ public class SingleTravelActivity extends AppCompatActivity {
 
         // setup single travel activity list view
         ListView listView_drawer = (ListView)findViewById(R.id.SingleTravelMapListView);
+        listView_drawer.setLongClickable(true);
+        listView_drawer.setOnItemLongClickListener(this);
         SingleTravelItemListViewAdapter singleTravelItemAdapter = new SingleTravelItemListViewAdapter(this);
         listView_drawer.setAdapter(singleTravelItemAdapter);
 
@@ -27,6 +47,24 @@ public class SingleTravelActivity extends AppCompatActivity {
         mapView = (MapView) findViewById(R.id.SingleTravelMapMapView);
         mapView.onCreate(savedInstanceState);
         aMap = mapView.getMap();
+        setupMap();
+    }
+
+    private void setupMap() {
+        aMap.setOnMapClickListener(this);
+    }
+
+    private void addMarkerToMap(LatLng location) {
+        markersLocation.add(location);
+        aMap.addMarker(new MarkerOptions().position(location));
+        linkMarkersOfMap();
+    }
+
+    private void linkMarkersOfMap() {
+        if (polyline != null) {
+            polyline.remove();
+        }
+        polyline = aMap.addPolyline(new PolylineOptions().addAll(markersLocation));
     }
 
     /**
@@ -63,5 +101,24 @@ public class SingleTravelActivity extends AppCompatActivity {
     public void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        Log.v("ekuri", latLng.toString());
+        addMarkerToMap(latLng);
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        View popViewContent = getLayoutInflater().inflate(R.layout.popup_window_for_single_travel_view_list_item, null);
+        PopupWindow popupWindow = new PopupWindow(popViewContent, ActionBar.LayoutParams.WRAP_CONTENT,
+                ActionBar.LayoutParams.WRAP_CONTENT, true);
+        popupWindow.setTouchable(true);
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.setBackgroundDrawable(new BitmapDrawable(getResources(), (Bitmap) null));
+        popupWindow.showAsDropDown(view);
+        Log.v("ekuri", "item long clicked");
+        return false;
     }
 }
