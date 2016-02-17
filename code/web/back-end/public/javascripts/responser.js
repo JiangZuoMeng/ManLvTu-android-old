0fd59c1f71032d9e257a17b06257153dfa06f50c
ekuri <ekuri>
2016-02-17 12:00:50 +0800

    seperate backend and html demo

--- code/web/back-end/public/javascripts/responser.js
+++
@@ -1,8 +0,0 @@
-function Responser () {
-	this.response = function (res, request_para, target_para, result_para, data_para) {
-		var result = {request : request_para, target : target_para, result : result_para, data : data_para};
-		res.json(result);
-	}
-}
-
-module.exports = Responser;
